25562812e3c76af5c12b0768090d91c1c9cfaca7
Udi <udiberes@gmail.com>
2025-02-05 23:17:20 +0200

    finished with Project4

---
+++ Project4/Fill.asm
@@ -0,0 +1,78 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/4/Fill.asm
+
+// Runs an infinite loop that listens to the keyboard input. 
+// When a key is pressed (any key), the program blackens the screen,
+// i.e. writes "black" in every pixel. When no key is pressed, 
+// the screen should be cleared.
+
+//// Replace this comment with your code.
+
+
+(MAIN_LOOP)
+
+    // Check if space pressed
+
+    @R0
+    M=0
+
+    @KBD
+    D=M
+
+    @32     // SPACE
+    D=D-A
+
+    // If space pressed set R0 to -1
+    @SET_R0_TO_BLACK    
+    D;JEQ   
+
+    @FILL_SCREEN
+    0;JMP
+
+    (SET_R0_TO_BLACK)
+        @R0
+        M=-1
+        @FILL_SCREEN
+        0;JMP
+
+    (FILL_SCREEN)
+
+        // Initialize the screen loop
+        @i
+        M=0
+        @SCREEN
+        D=A
+        @n
+        M=D
+        @addr   // Set addr to the screen base address
+        M=D
+
+        (FILL_SCREEN_LOOP)
+            @i
+            D=M
+            @n
+            D=M-D
+            @MAIN_LOOP
+            D;JEQ
+            
+            // Set the pixel according to the press value
+            @R0
+            D=M
+            @addr
+            A=M
+            M=D
+
+            @i
+            M=M+1   // i++
+            @addr
+            M=M+1  // addr = addr + 1
+
+            @FILL_SCREEN_LOOP
+            0;JMP
+
+    @MAIN_LOOP
+    0;JMP
+
+
