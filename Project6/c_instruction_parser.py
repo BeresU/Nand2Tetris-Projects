d000330b953dd837eb356f41d99fed1162075572
Udi <udiberes@gmail.com>
2025-02-20 16:02:05 +0200

    finished with c_instruction_extractor + rename

---
+++ Project6/c_instruction_parser.py
@@ -0,0 +1,64 @@
+DEST_MAP = {
+    "null": "000",
+    "M": "001",
+    "D": "010",
+    "DM": "011",
+    "A": "100",
+    "AM": "101",
+    "AD": "110",
+    "ADM": "111",
+}
+
+COMP_MAP = {
+    "0": "0101010",
+    "1": "0111111",
+    "-1": "0111010",
+    "D": "0001100",
+    "A": "0110000",
+    "M": "1110000",
+    "!D": "0001101",
+    "!A": "0110001",
+    "!M": "1110001",
+    "-D": "0001111",
+    "-A": "0110011",
+    "-M": "1110011",
+    "D+1": "0011111",
+    "A+1": "0110111",
+    "M+1": "1110111",
+    "D-1": "0001110",
+    "A-1": "0110010",
+    "M-1": "1110010",
+    "D+A": "0000010",
+    "D+M": "1000010",
+    "D-A": "0010011",
+    "D-M": "1010011",
+    "A-D": "0000111",
+    "M-D": "1000111",
+    "D&A": "0000000",
+    "D&M": "1000000",
+    "D|A": "0010101",
+    "D|M": "1010101",
+}
+
+JUMP_MAP = {
+    "null": "000",
+    "JGT": "001",
+    "JEQ": "010",
+    "JGE": "011",
+    "JLT": "100",
+    "JNE": "101",
+    "JLE": "110",
+    "JMP": "111",
+}
+
+
+def parse(line):
+    split_jmp = line.split(";")
+    split_dst = split_jmp[0].split("=")
+    
+    jmp_bits = JUMP_MAP["null"] if len(split_jmp) == 1 else JUMP_MAP[split_jmp[1]]
+    comp_bits = COMP_MAP[split_dst[0]] if len(split_dst) == 1 else COMP_MAP[split_dst[1]]
+    dest_bits = DEST_MAP["null"] if len(split_dst) == 1 else DEST_MAP[split_dst[0]]
+
+    return f"111{comp_bits}{dest_bits}{jmp_bits}"
+
