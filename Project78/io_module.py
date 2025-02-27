605781000e5d5baa0e74aea937fd99b54043b322
Udi <udiberes@gmail.com>
2025-02-27 19:48:22 +0200

    added io module

---
+++ Project78/io_module.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+from collections import namedtuple
+
+VmData = namedtuple("VmData", ["file_name", "data"])
+
+
+def get_vm_file_data(path):
+    file_path = Path(path)
+    print(f"opening: ${file_path}")
+    with open(file_path, "r", encoding="utf-8") as file:
+        return VmData(file_path.stem, file.readlines())
+
+
+def create_assembly_file(file_name, lines, target_path):
+    output_path = Path(target_path) / f"{file_name}".asm
+    counter = 0
+
+    while output_path.exists():
+        output_path = output_path / f"{file_name}{counter}.asm"
+        counter += 1
+
+    output_path.parent.mkdir(parents=True, exist_ok=True)
+
+    output_path.write_text("\n".join(lines), encoding="utf-8")
