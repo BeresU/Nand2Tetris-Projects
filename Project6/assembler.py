c4152c426fd10b4e797cfd4b032d59eab8bdc800
Udi <udiberes@gmail.com>
2025-02-27 14:24:02 +0200

    renames and cleanings in Project6

--- Project6/assembler.py
+++ Project6/assembler.py
@@ -1,26 +1,47 @@
 import binary_converter
+
 import c_instruction_parser
+
 import line_parser
+
 from line_parser import ParseResults
+
 from line_parser import ParseResultsType
 
-def parse_lines(file_lines, symbol_table):
+
+def parse_file(file_lines, symbol_table):
+
     parsed_lines = []
 
+
     for line in file_lines:
+
         line_results = line_parser.parse_line(line)
 
+
         match line_results.resultsType:
+
             case ParseResultsType.EMPTY | ParseResultsType.LABEL:
                 continue
+
             case ParseResultsType.CONSTANT:
+
                 constant_to_binary = binary_converter.convert_to_binary(int(line_results.resultsData))
+
                 parsed_lines.append(constant_to_binary)
+
             case ParseResultsType.VARIABLE:
+
                 variable_data = symbol_table.get(line_results.resultsData)
+
                 parsed_lines.append(variable_data)
+
             case ParseResultsType.C_INSTRUCTION:
+
                 c_instruction_results = c_instruction_parser.parse(line_results.resultsData)
+
                 parsed_lines.append(c_instruction_results)
 
+
     return parsed_lines
+
--- Project6/main.py
+++ Project6/main.py
@@ -1,20 +1,20 @@
 from symbol_table import SymbolTable
 import io_module
 from io_module import AsmData
-
 import symbol_extractor
-import parser
+import assembler
 import concurrent.futures
 
 
-def parse_and_create_file(data, file_name):
+def parse_and_create_file(file_lines, file_name):
     table = SymbolTable()
-    symbol_extractor.extract_symbols(data, table)
-    parsed_lines = parser.parse_lines(data, table)
-    io_module.create_hack_file(file_name, parsed_lines)
+    symbol_extractor.extract_symbols(file_lines, table)
+    parsed_content = assembler.parse_file(file_lines, table)
+    io_module.create_hack_file(file_name, parsed_content)
 
 
 asm_input_files = io_module.get_asm_file_lines()
+
 
 with concurrent.futures.ThreadPoolExecutor(8) as executor:
     futures = {
@@ -25,4 +25,4 @@ with concurrent.futures.ThreadPoolExecutor(8) as executor:
     }
 
     for future in concurrent.futures.as_completed(futures):
-        future.result()  
+        future.result()
