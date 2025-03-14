b81478ecc0bacbfa1bdcc4c854248681915fd7c8
Udi <udiberes@gmail.com>
2025-03-14 21:47:42 +0200

    added BasicLoop.asm

---
+++ Project78/Input/Project8/BasicLoop.asm
@@ -0,0 +1,146 @@
+// Passed 
+
+// push: constant: 0
+@0
+D=A
+
+// RAM[SP] = D, SP++
+@SP
+AM=M+1
+A=A-1
+M=D
+
+// pop: LCL 0
+@LCL
+D=M
+@R13
+M=D
+
+// SP--
+@SP
+AM=M-1
+D=M
+@R13
+A=M
+M=D
+(BasicLoop$LOOP)
+
+// push: ARG 0
+@ARG
+A=M
+D=M
+
+// RAM[SP] = D, SP++
+@SP
+AM=M+1
+A=A-1
+M=D
+
+// push: LCL 0
+@LCL
+A=M
+D=M
+
+// RAM[SP] = D, SP++
+@SP
+AM=M+1
+A=A-1
+M=D
+
+// do add
+
+// SP--
+@SP
+AM=M-1
+D=M
+A=A-1
+M=D+M
+
+// pop: LCL 0
+@LCL
+D=M
+@R13
+M=D
+
+// SP--
+@SP
+AM=M-1
+D=M
+@R13
+A=M
+M=D
+
+// push: ARG 0
+@ARG
+A=M
+D=M
+
+// RAM[SP] = D, SP++
+@SP
+AM=M+1
+A=A-1
+M=D
+
+// push: constant: 1
+@1
+D=A
+
+// RAM[SP] = D, SP++
+@SP
+AM=M+1
+A=A-1
+M=D
+
+// do sub
+
+// SP--
+@SP
+AM=M-1
+D=M
+A=A-1
+M=M-D
+
+// pop: ARG 0
+@ARG
+D=M
+@R13
+M=D
+
+// SP--
+@SP
+AM=M-1
+D=M
+@R13
+A=M
+M=D
+
+// push: ARG 0
+@ARG
+A=M
+D=M
+
+// RAM[SP] = D, SP++
+@SP
+AM=M+1
+A=A-1
+M=D
+
+// if-goto BasicLoop$LOOP
+
+// SP--
+@SP
+AM=M-1
+D=M
+@BasicLoop$LOOP
+D;JNE
+
+// push: LCL 0
+@LCL
+A=M
+D=M
+
+// RAM[SP] = D, SP++
+@SP
+AM=M+1
+A=A-1
+M=D
