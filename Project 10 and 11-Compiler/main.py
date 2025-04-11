7a41459621f60723fb11c62ab0125c035af227d2
Udi <udiberes@gmail.com>
2025-04-11 14:10:21 +0300

    added to main parse cmd handling

--- Project 10 and 11-Compiler/main.py
+++ Project 10 and 11-Compiler/main.py
@@ -1,1 +1,17 @@
-print("test")
+import argparse
+import os
+from pathlib import Path
+
+def main():
+    parser = argparse.ArgumentParser(description="Process a file path.")
+    parser.add_argument("path", type=str, help="The path to the file")
+    parser.add_argument("-o", "--output", type=str, help="The output file path")
+
+    args = parser.parse_args()
+    full_path = os.path.abspath(args.path)
+    output_path = Path(full_path).parent
+
+    print(f"Args: {full_path}")
+
+if __name__ == '__main__':
+    main()
