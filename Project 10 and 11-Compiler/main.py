0f3b5f6e973776552582dc851bf5cd56ef6b2f69
Udi <udiberes@gmail.com>
2025-04-14 14:14:51 +0300

    fixed output path

--- Project 10 and 11-Compiler/main.py
+++ Project 10 and 11-Compiler/main.py
@@ -3,18 +3,21 @@ import os
 from pathlib import Path
 import jack_analyzer
 
+
 def main():
     parser = argparse.ArgumentParser(description="Process a file path.")
     parser.add_argument("path", type=str, help="The path to the file")
     parser.add_argument("-o", "--output", type=str, help="The output file path")
 
     args = parser.parse_args()
-    input_path = os.path.abspath(args.path)
-    output_path = Path(input_path).parent if args.output is None else args.output
+    input_path = Path(os.path.abspath(args.path))
+
+    output_path = input_path.parent / f"{input_path.stem}_output" if args.output is None else args.output
 
     print(f"Args: input path: {input_path}, output path: {output_path}")
 
-    jack_analyzer.analyze(input_path, output_path)
+    jack_analyzer.analyze(str(input_path), output_path)
+
 
 if __name__ == '__main__':
     main()
