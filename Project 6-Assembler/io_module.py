c64ae9018c7c2711a23c4846f4da63bac33a7ed4
Udi <udiberes@gmail.com>
2025-04-18 14:23:04 +0300

    refactored the code to follow the standard of the other projects

--- Project 6-Assembler/io_module.py
+++ Project 6-Assembler/io_module.py
@@ -4,31 +4,39 @@ from collections import namedtuple
 AsmData = namedtuple("AsmData", ["file_name", "data"])
 
 
-def get_asm_file_lines():
-    parent_path = Path(__file__).parent
-    input_path = parent_path / "Input"
+def get_files_data(path):
+    files_data = []
 
-    files = [f for f in input_path.iterdir() if f.is_file()]
-    asm_files_data = []
+    data_path = Path(path)
 
-    for file_path in files:
-        print(f"opening: ${file_path}")
-        with open(file_path, "r", encoding="utf-8") as file:
-            asm_data = AsmData(file_path.stem, file.readlines())
-            asm_files_data.append(asm_data)
+    if data_path.is_file():
+        vm_file_data = get_asm_file(path)
+        files_data.append(vm_file_data)
+    else:
+        vm_files = list(data_path.glob("*.asm"))
 
-    return asm_files_data
+        for vm_file in vm_files:
+            vm_file_data = get_asm_file(vm_file)
+            files_data.append(vm_file_data)
+
+    return files_data
 
 
-def create_hack_file(file_name, lines):
-    parent_path = Path(__file__).parent
-    output_path = parent_path / "Output" / f"{file_name}.hack"
+def get_asm_file(path):
+    file_path = Path(path)
+    print(f"opening: ${file_path}")
+    with open(file_path, "r", encoding="utf-8") as file:
+        return AsmData(file_path.stem, file.readlines())
+
+
+def create_hack_file(file_name, lines, output_path):
+    output_path_obj = Path(output_path) / f"{file_name}.hack"
 
     counter = 0
-    while output_path.exists():
-        output_path = parent_path / "Output" / f"{file_name}{counter}.hack"
+    while output_path_obj.exists():
+        output_path_obj = Path(output_path) / f"{file_name}_{counter}.hack"
         counter += 1
 
-    output_path.parent.mkdir(parents=True, exist_ok=True)
+    output_path_obj.parent.mkdir(parents=True, exist_ok=True)
 
-    output_path.write_text("\n".join(lines), encoding="utf-8")
+    output_path_obj.write_text("\n".join(lines), encoding="utf-8")
--- Project 6-Assembler/main.py
+++ Project 6-Assembler/main.py
@@ -1,3 +1,6 @@
+import argparse
+import os
+from pathlib import Path
 from symbol_table import SymbolTable
 import io_module
 import symbol_extractor
@@ -9,17 +12,28 @@ def parse_and_create_file(file_lines, file_name):
     table = SymbolTable()
     symbol_extractor.extract_symbols(file_lines, table)
     parsed_content = assembler.parse_file(file_lines, table)
-    io_module.create_hack_file(file_name, parsed_content)
+    io_module.create_hack_file(file_name, parsed_content, output_path)
 
+parser = argparse.ArgumentParser(description="Process a file path.")
+parser.add_argument("path", type=str, help="The path to the file")
+parser.add_argument("-o", "--output", type=str, help="The output file path")
 
-asm_input_files = io_module.get_asm_file_lines()
+args = parser.parse_args()
+full_path = os.path.abspath(args.path)
+input_path_obj = Path(full_path)
+
+output_path = input_path_obj if args.output is None else args.output
+
+print(f"Args: input path: {input_path_obj}, output path: {output_path}")
+
+asm_files_data = io_module.get_files_data(input_path_obj)
 
 with concurrent.futures.ThreadPoolExecutor(8) as executor:
     futures = {
         executor.submit(
             parse_and_create_file, asm_input_file.data, asm_input_file.file_name
         ): asm_input_file
-        for asm_input_file in asm_input_files
+        for asm_input_file in asm_files_data
     }
 
     for future in concurrent.futures.as_completed(futures):
